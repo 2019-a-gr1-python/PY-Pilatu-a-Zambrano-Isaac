a8e8d48b7f8c83285b23458860d36189f25f0465
IsaacPilatuna <isapila1996@hotmail.com>
2019-06-08 10:49:00 -0500

    Recorrer directorios

--- 03_Pandas/D_lectura_json.py
+++ 03_Pandas/D_lectura_json.py
@@ -28,11 +28,35 @@ df_small_2=pd.DataFrame(
         )
 
 
-def leer_json(path,llaves):
+def leer_json(path_archivo,llaves):
     with open(path_archivo) as texto_json:
-        contenido_json=json.load(texto_json)
-    registro_df_lista=[]
-    for key in llaves:
-        valor=contenido_json[key]
-        registro_df.append(valor)
+        contenido_json = json.load(texto_json)
+    registro_df_lista = []
+    for llave in llaves:
+        valor = contenido_json[llave]
+        registro_df_lista.append(valor)
     return registro_df_lista
+
+def leer_json_en_carpetas(directorio, llaves):
+    trabajos_arte=[]
+    for path_raiz, lista_directorios, archivos in os.walk(directorio):
+               
+        for nombre_archivo in archivos:
+            if (nombre_archivo.endswith('json')):
+                directorio_archivo=os.path.join(path_raiz,nombre_archivo)
+                pieza_arte = leer_json(directorio_archivo,llaves)
+                print(directorio_archivo)
+                trabajos_arte.append(pieza_arte)
+                
+    df=pd.DataFrame.from_records(
+            trabajos_arte,
+            columns=llaves,
+            index='id'
+            )
+    return df
+
+df_artworks=leer_json_en_carpetas(path_general,llaves)
+
+
+
+    
--- 03_Pandas/G_llenar_valores.py
+++ 03_Pandas/G_llenar_valores.py
@@ -15,18 +15,11 @@ for aquisitionYear, registros in df_agrupado_ay:
     print(aquisitionYear)
     print(registros)
     
-def llenar_valores_vacios(serie):
-    valores=serie.value_counts()
+def llenar_valores_vacios(series):
+    valores = series.value_counts()
     if(valores.empty):
-        return serie
-    else:
-        valor_nuevo='N/A'
-        for i in serie:
-            if(i==''):
-                serie[i]=valor_nuevo
-        return serie
-        
-"""   
+        return series
+    """   
     # 1) iterar y sumar los valores
     sumatoria = 0
     numero_nans = 0
@@ -44,19 +37,35 @@ def llenar_valores_vacios(serie):
     valor_mas_utilizado = sumatoria / division
     print(valor_mas_utilizado)
     """
-    
+    nuevo_valor = series.fillna(valores.index[0])
+    return nuevo_valor
+
 def transformar_df(df):
-    df_artist=df.groupby('artist')
-    arreglo_df_grupo=[]
-    for nombre_artista, registros in df_artist:
-        copia=registros.copy()
-        serie_medium=registros['medium']
-        serie_units=registros['units']
+    df_artist = df.groupby('artist')
+    arreglo_df_grupo = []
+    
+    for nombre_artista, registros_agrupados in df_artist:
+        copia = registros_agrupados.copy()
+        serie_medium = registros_agrupados['medium']
+        serie_units = registros_agrupados['units']
         copia.loc[:,'medium'] = llenar_valores_vacios(serie_medium)
         copia.loc[:,'units'] = llenar_valores_vacios(serie_units)
         arreglo_df_grupo.append(copia)
-        
-    nuevo_df_transformado=pd.concat(arreglo_df_grupo)
+    
+    nuevo_df_transformado = pd.concat(arreglo_df_grupo)
     return nuevo_df_transformado
 
-seccion_df_t=transformar_df(seccion_df)
+
+seccion_df_t = transformar_df(seccion_df)
+
+df_agrupado_titulo=df.groupby('title')
+print(df_agrupado_titulo.size())
+
+
+
+serie_titulos=df_agrupado_titulo.size().sort_values(ascending=False)
+df_filtrado=df.filter(items=["artist","title"])
+condicion=lambda x: len(x.index)>1
+df_titulos_dup=df_agrupado_titulo.filter(condicion)
+resultado =df_titulos_dup.sort_values('title', inplace=True)
+
